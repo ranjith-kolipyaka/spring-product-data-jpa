ac69f1ddac509a73e892075eddf672e992400039
Ranjith Reddy Kolipyaka <ranjith@Ranjiths-MacBook-Pro.local>
2023-03-13 12:23:18 -0400

    New addition

---
+++ src/main/java/com/learning/spring/jpa/springproductdatajpa/SpringProductDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.jpa.springproductdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProductDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProductDataJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring/jpa/springproductdatajpa/SpringProductDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.jpa.springproductdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProductDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
